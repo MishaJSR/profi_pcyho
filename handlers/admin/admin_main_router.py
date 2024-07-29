16309ae20fe458b35470a1d990d01420cfe90bf8
Michail S <mixan_45@mail.ru>
2024-07-29 11:04:04 +0300

    start 1.61

--- handlers/admin/admin_main_router.py
+++ handlers/admin/admin_main_router.py
@@ -5,6 +5,7 @@ from dotenv import find_dotenv, load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.orm_query_user import delete_me_user
+from database.orm_query_user_task_progress import delete_all_user_progress
 from filters.admin_filter import AdminFilter
 from handlers.admin.admin_excel_loader_router import admin_excel_loader_router
 from handlers.admin.admin_manage_sender_router import admin_manage_sender_router
@@ -31,6 +32,7 @@ async def fill_admin_state(message: types.Message, state: FSMContext):
 @admin_private_router.message(F.text == "Удалить меня")
 async def fill_admin_state(message: types.Message, session: AsyncSession,  state: FSMContext):
     await delete_me_user(session, user_id=message.from_user.id)
+    await delete_all_user_progress(session, user_id=message.from_user.id)
     await message.answer(text="Удалено", reply_markup=start_kb())
 
 
